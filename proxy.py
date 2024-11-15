3510450fa3f3840b5f12645d470ca13273764ea9
AmarjotS82 <amarjot.sangha28@gmail.com>
2024-11-14 21:49:51 -0800

    comments for delay/drop

--- proxy.py
+++ proxy.py
@@ -14,7 +14,7 @@ def start_proxy(self, proxy_params):
     server_delay_rate = proxy_params.client_delay_rate
     client_delay_time = proxy_params.client_delay_time
     server_delay_time = proxy_params.server_delay_time
-    
+
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.bind((listen_ip_addr, listen_port_num))
     print(f"Proxy send/recv to client on {listen_ip_addr}:{listen_port_num}")
@@ -27,11 +27,18 @@ def start_proxy(self, proxy_params):
         # message recieved from client
         message, client_address = client_socket.recvfrom(1024)  # buffer size 1024 bytes
         print(f"Received message: {message.decode()} from {client_address}")
+        # Add client delay and drop functions here to do before sending 
+
         server_socket.sendto(message.encode(), (target_ip_addr, target_port_num))
+
+        
+
 
         # server ACK response
         message, server_address = server_socket.recvfrom(1024)  # buffer size 1024 bytes
         print(f"Received ack: {message.decode()} from {server_address}")
+        # Add server delay and drop functions here to do before sending 
+        
         client_socket.send(message)
 
 if __name__ == "__main__":
